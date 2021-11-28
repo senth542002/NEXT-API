8834c005d1360298831c552f7ca0a77cb9aa1967
Senthil Kumar <vsenth17@ford.com>
2021-11-28 16:12:59 +0530

    Saving Comment using Next - POST API

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -2,6 +2,18 @@ import { comments } from "../../../data/comments";
 import { commentsData } from '../../../data/comments'
 
 export default function handler(req,res) {
+
+    if(req.method === 'GET') {
+        res.status(200).json(commentsData)
+    } else if(req.method === 'POST') {
+        const comment = req.body.comment
+        const newComment = {
+            id: Date.now(),
+            text: comment
+        }
+        commentsData.push(newComment)
+
+        res.status(201).json(newComment)
+    }
     console.log(commentsData)
-    res.status(200).json(commentsData)
 }
--- pages/comments/index.js
+++ pages/comments/index.js
@@ -3,6 +3,7 @@ import {useState} from 'react'
 function CommentsPage() {
 
     const [comments, setComments] = useState([])
+    const [comment, setComment] = useState('')
 
     const fetchComments = async () => {
         const response = await fetch(`/api/comments`)
@@ -10,8 +11,20 @@ function CommentsPage() {
         setComments(data)
     }
 
+    const submitComment = async () => {
+        const respose = await fetch('/api/comments',{
+            method: 'POST',
+            body: JSON.stringify({comment}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    }
+
     return(
         <>
+        <input type='text' value={comment} onChange={(e) => setComment(e.target.value)}></input>
+        <button onClick={submitComment}>Submit Comment</button>
         <button onClick={fetchComments}>Load Comments</button> <br/><br/>
         {
             comments.map(comment => {
